9342bb96d3b3ef03217aa414cdc170cfedc04970
Danison <danchetirkin00711@gmail.com>
2026-01-18 19:19:17 +0200

    Starred route so it will be in down menu and added IP so we can connect from our own phone(we need to write our computer IP in const LAN_IP)

--- mobile/api/config.js
+++ mobile/api/config.js
@@ -1,2 +1,12 @@
 //configuration for the API connection
-export const API_BASE = "http://10.0.2.2:5000/api";
+import { Platform } from "react-native";
+import * as Device from "expo-device";
+
+const LAN_IP = "172.16.22.26";
+
+const isAndroidEmulator =
+  Platform.OS === "android" && !Device.isDevice;
+
+export const API_BASE = isAndroidEmulator
+  ? "http://10.0.2.2:5000/api"
+  : `http://${LAN_IP}:5000/api`;
